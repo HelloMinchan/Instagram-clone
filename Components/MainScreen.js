014e8bfe7e4731be6f18c663e189f59f174ba911
HelloMinchan <wjdalscksdle@naver.com>
2019-11-09 18:37:13 +0900

    2019.11.09

---
+++ Components/MainScreen.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { Icon } from 'native-base';
+import { createAppContainer } from 'react-navigation';
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs'
+import HomeTab from './AppTabNavigator/HomeTab'
+import SearchTab from './AppTabNavigator/SearchTab'
+import AddMediaTab from './AppTabNavigator/AddMediaTab'
+import LikesTab from './AppTabNavigator/LikesTab'
+import ProfileTab from './AppTabNavigator/ProfileTab'
+
+const AppTabNavigator = createMaterialTopTabNavigator({
+  HomeTab: { screen: HomeTab },
+  SearchTab: { screen: SearchTab },
+  AddMediaTab: { screen: AddMediaTab },
+  LikesTab: { screen: LikesTab },
+  ProfileTab: { screen: ProfileTab }
+}, {
+  animationEnabled: true,
+  swipeEnabled: true,
+  tabBarPosition: "bottom",
+  tabBarOptions: {
+    style: {
+      ...Platform.select({
+        android:{
+          backgroundColor:'white',
+        }
+      })
+    },
+    iconStyle: { height: 30 },
+    activeTintColor: '#000',
+    inactiveTintColor: '#d1cece',
+    upperCaseLabel: false,
+    showLabel: false,
+    showIcon: true,
+  }
+});
+
+const AppTabContainet = createAppContainer(AppTabNavigator);
+
+export default class MainScreen extends Component {
+  // navigationOptions 코드 추가
+  static navigationOptions = {
+    headerLeft: <Icon name='ios-camera' style={{ paddingLeft:10 }}/>,
+    title: 'Instagram',
+    headerRight: <Icon name='ios-send' style={{ paddingRight:10 }}/>,
+  }
+
+  render() {
+    return (
+      <AppTabContainet/>
+    );
+  }
+}
